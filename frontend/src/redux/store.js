1c4dff711b0ac9ee9f865868b2533669b122512d
onyebuchi702 <emechebeonyebuchi@mail.com>
2021-05-07 11:45:51 +0200

    initial commit

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const initialState = {
+}
+
+const reducer = combineReducers({
+})
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store
